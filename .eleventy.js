c12f2fb678d7d84db1d0d4626fa997c65aa5efa3
Jens Andreasson <jensandreasson77@gmail.com>
2021-12-01 09:43:09 +0100

    ✨ sort pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,21 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 module.exports = (eleventyConfig) => {
     eleventyConfig.addPlugin(syntaxHighlight);
-    
-    eleventyConfig.addWatchTarget("./src/assets/");
-    eleventyConfig.addWatchTarget("./src/js/");
 
-    eleventyConfig.addPassthroughCopy("./src/assets/");
-    eleventyConfig.addPassthroughCopy("./src/js/");
+    eleventyConfig.addWatchTarget('./src/assets/');
+    eleventyConfig.addWatchTarget('./src/js/');
+
+    eleventyConfig.addPassthroughCopy('./src/assets/');
+    eleventyConfig.addPassthroughCopy('./src/js/');
 
     // Collections
-    eleventyConfig.addCollection('pages', (collectionApi) =>
-        collectionApi.getFilteredByGlob('src/pages/*.njk')
-    );
+    eleventyConfig.addCollection('pages', (collectionApi) => {
+        return collectionApi
+            .getFilteredByGlob('src/pages/*.njk')
+            .sort((a, b) => {
+                return a.data.title.localeCompare(b.data.title);
+            });
+    });
 
     return {
         markdownTemplateEngine: 'njk',
