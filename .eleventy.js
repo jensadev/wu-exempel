2892794d7796c1c3a3055769bcef2c0645d3ce1c
Jens Andreasson <jens.andreasson@ntig.se>
2022-01-11 12:58:52 +0100

    🚸 404

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,7 @@ module.exports = (eleventyConfig) => {
 
     // Filters
     glob.sync(['src/filters/*.js']).forEach((file) => {
-        let filters = require('./' + file);
+        const filters = require(`./${file}`);
         Object.keys(filters).forEach((name) =>
             eleventyConfig.addFilter(name, filters[name])
         );
@@ -31,6 +31,25 @@ module.exports = (eleventyConfig) => {
                 }
                 return a.data.title.localeCompare(b.data.title);
             });
+    });
+
+
+    // 404
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: (err, bs) => {
+                bs.addMiddleware('*', (req, res) => {
+                    const content_404 = fs.readFileSync('docs/404.html');
+                    // Add 404 http status code in request header.
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html; charset=UTF-8',
+                    });
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            },
+        },
     });
 
     return {
